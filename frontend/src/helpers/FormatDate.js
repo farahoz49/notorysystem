fc1dfbbb9465a6c55c901ab188e12d56d6968fb9
farah ugaas <farahugaas0@gmail.com>
2026-03-19 10:24:32 +0300

    for production

--- frontend/src/helpers/FormatDate.js
+++ frontend/src/helpers/FormatDate.js
@@ -10,4 +10,29 @@ export const formatDate = (dateString) => {
       return `${day}/${month}/${year}`;
     };
 
- 
+    const months = [
+  "Jannaayo",
+  "Febraayo",
+  "Maarso",
+  "Abriil",
+  "Maayo",
+  "Juuno",
+  "Luulyo",
+  "Agoosto",
+  "Sebteembar",
+  "Oktoobar",
+  "Nofeembar",
+  "Diseembar",
+];
+
+export const formatDatewithname = (dateValue) => {
+  if (!dateValue) return "";
+
+  const d = new Date(dateValue);
+
+  const day = String(d.getDate()).padStart(2, "0");
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+};
